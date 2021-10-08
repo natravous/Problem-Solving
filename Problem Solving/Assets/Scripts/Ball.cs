afd61508b00d1ea29bb3a1dd5d76f4e0b40f72df
natravous <67213861+natravous@users.noreply.github.com>
2021-10-08 19:31:26 +0700

    Problem 2

--- Problem Solving/Assets/Scripts/Ball.cs
+++ Problem Solving/Assets/Scripts/Ball.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D rb;
+
+    public float speed = 5f;
+
+    private Vector2 movement;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float rand = Random.RandomRange(0, 4);
+        movement.x = rand;
+        movement.y = rand;
+        Debug.Log(movement);
     }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + (movement * speed * Time.deltaTime));
+    }
+
 }
