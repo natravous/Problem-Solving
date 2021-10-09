250466e6acea7fa8d4815f808f3044fd186c031d
natravous <67213861+natravous@users.noreply.github.com>
2021-10-09 14:11:48 +0700

    Problem 5

--- Problem Solving/Assets/Scripts/Ball.cs
+++ Problem Solving/Assets/Scripts/Ball.cs
@@ -9,25 +9,35 @@ public class Ball : MonoBehaviour
     public float speed = 5f;
 
     private Vector2 movement;
+    private Vector3 mousePosition;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
         //
         //rb.velocity = new Vector2(5f, 5f);
-       
+        movement = new Vector2(0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
+
+
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        //movement = Vector2.Lerp(transform.position, mousePosition, speed);
     }
 
     private void FixedUpdate()
     {
+        rb.transform.position = Vector2.MoveTowards(transform.position, mousePosition, Time.deltaTime * speed);
+
         LauchBall();
     }
 
@@ -39,6 +49,7 @@ public class Ball : MonoBehaviour
         //Debug.Log(movement);
 
         rb.MovePosition(rb.position + (movement * speed * Time.deltaTime));
+        //rb.MovePosition(movement);
 
     }
 
