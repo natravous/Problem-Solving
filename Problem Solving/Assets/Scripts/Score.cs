c0aaea9f470caedcab374baeadcbe35543fb1462
natravous <67213861+natravous@users.noreply.github.com>
2021-10-09 15:06:09 +0700

    Problem 7

---
+++ Problem Solving/Assets/Scripts/Score.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Transform ball;
+    private Text scoreText;
+    public static int scoreValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreValue = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Score: " + scoreValue.ToString();
+    }
+}
