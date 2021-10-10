b46a4c9a3ef975829fcdb9fb9a9515332bcfb00d
natravous <67213861+natravous@users.noreply.github.com>
2021-10-10 16:51:50 +0700

    Problem 10

---
+++ Problem Solving/Assets/Scripts/Ball/BallSeven.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSeven : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public float speed = 5f;
+
+    private Vector2 movement;
+    private Vector3 mousePosition;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        movement = new Vector2(0f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
+
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.transform.position = Vector2.MoveTowards(transform.position, mousePosition, Time.deltaTime * speed);
+
+        LauchBall();
+    }
+
+    public void LauchBall()
+    {
+        rb.MovePosition(rb.position + (movement * speed * Time.deltaTime));
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Box")
+        {
+            Score.scoreValue++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Problem Solving/Assets/Scripts/UIManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void Back()
+    {
+        SceneManager.LoadScene("Problem 10");
+    }
+    public void Problem1()
+    {
+        SceneManager.LoadScene("Problem 1");
+    }
+    public void Problem2()
+    {
+        SceneManager.LoadScene("Problem 2");
+    }
+    public void Problem3()
+    {
+        SceneManager.LoadScene("Problem 3");
+    }
+    public void Problem4()
+    {
+        SceneManager.LoadScene("Problem 4");
+    }
+    public void Problem5()
+    {
+        SceneManager.LoadScene("Problem 5");
+    }
+    public void Problem6()
+    {
+        SceneManager.LoadScene("Problem 6");
+    }
+    public void Problem7()
+    {
+        SceneManager.LoadScene("Problem 7");
+    }
+    public void Problem8()
+    {
+        SceneManager.LoadScene("Problem 8");
+    }
+    public void Problem9()
+    {
+        SceneManager.LoadScene("Problem 9");
+    }
+}
